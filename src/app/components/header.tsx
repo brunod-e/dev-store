111e0d5ae809a403181e10818530625b9cf4ecdb
Bruno <brunodaniel.elias@gmail.com>
2023-11-20 16:50:57 -0300

    create: header component

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,3 +1,43 @@
+import Link from "next/link"
+import { Search, ShoppingBag } from "lucide-react"
+import Image from "next/image"
+
 export default function Header() {
-  return <div>Header</div>
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex items-center gap-5">
+        <Link href="/" className="text-2xl font-extrabold text-white">
+          devstore_
+        </Link>
+
+        <form className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700">
+          <Search className="w-5 h-6 text-zinc-500" size={24} />
+          <input
+            type="text"
+            placeholder="Buscar produtos..."
+            className="flex-1 bg-transparent outline-none text-sm outine-none placeholder:text-zinc-500"
+          />
+        </form>
+      </div>
+      <div className="flex items-center gap-4">
+        <div className="flex items-center gap-2">
+          <ShoppingBag className="h-4 w-4" />{" "}
+          <span className="text-sm">Cart (0)</span>
+        </div>
+
+        <div className="w-px h-4 bg-zinc-700" />
+
+        <Link href="/" className="flex items-center gap-2 hover:underline">
+          <span className="text-sm">Account</span>
+          <Image
+            src="https://github.com/brunod-e.png"
+            className="h-6 w-6 rounded-full"
+            width={24}
+            height={24}
+            alt="Imagem do usuário"
+          />
+        </Link>
+      </div>
+    </div>
+  )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css"
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" })
 
 export const metadata: Metadata = {
-  title: "Dev Store",
+  title: "devstore_",
 }
 
 export default function RootLayout({
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html className={inter.variable} lang="pt">
-      <body>{children}</body>
+      <body className="bg-zinc-950 text-zinc-50 antialiased">{children}</body>
     </html>
   )
 }
