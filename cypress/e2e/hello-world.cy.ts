524473042339ebbda1d64a96407d49dd54d17c35
Bruno <brunodaniel.elias@gmail.com>
2023-11-21 19:25:01 -0300

    create: cypress add to cart

--- cypress/e2e/hello-world.cy.ts
+++
@@ -1,8 +0,0 @@
-describe("template spec", () => {
-  it("passes", () => {
-    cy.visit("http://rocketseat.com.br")
-    cy.contains("Quero assinar agora", {
-      matchCase: false,
-    }).click()
-  })
-})
