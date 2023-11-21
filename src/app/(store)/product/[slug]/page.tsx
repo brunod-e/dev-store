bf4a2e7b2671a80e0995868316d033411e762728
Bruno <brunodaniel.elias@gmail.com>
2023-11-21 15:41:04 -0300

    feat: add product to cart

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { AddToCartButton } from "@/components/add-to-cart-button"
 import { api } from "@/data/api"
 import { Product } from "@/data/types/product"
 import { Metadata } from "next"
@@ -108,12 +109,7 @@ export default async function ProductPage({ params }: ProductProps) {
           </div>
         </div>
 
-        <button
-          type="button"
-          className="mt-8 flex h-12 items-center justify-center rounded-full bg-emerald-600 font-semibold text-white"
-        >
-          Adicionar ao carrinho
-        </button>
+        <AddToCartButton productId={product.id} />
       </div>
     </div>
   )
---
+++ src/components/add-to-cart-button.tsx
@@ -0,0 +1,24 @@
+"use client"
+import { useCart } from "@/contexts/cart-context"
+
+export interface AddToCartButtonProps {
+  productId: number
+}
+
+export function AddToCartButton({ productId }: AddToCartButtonProps) {
+  const { addToCart } = useCart()
+
+  function handleAddToCart() {
+    addToCart(productId)
+  }
+
+  return (
+    <button
+      type="button"
+      className="mt-8 flex h-12 items-center justify-center rounded-full bg-emerald-600 font-semibold text-white"
+      onClick={handleAddToCart}
+    >
+      Adicionar ao carrinho
+    </button>
+  )
+}
