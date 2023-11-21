8fcd0bccf8b07ae76f1de0c170b47d2fb0ad37f1
Bruno <brunodaniel.elias@gmail.com>
2023-11-20 22:22:22 -0300

    create: product details page

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -20,7 +20,7 @@ export default async function Home() {
   return (
     <div className="grid max-h-[830px] grid-cols-9 grid-rows-6 gap-6">
       <Link
-        href={`/products/${heroProduct.slug}`}
+        href={`/product/${heroProduct.slug}`}
         className="group relative col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
       >
         <Image
@@ -47,7 +47,7 @@ export default async function Home() {
         return (
           <Link
             key={product.id}
-            href={`/products/${product.slug}`}
+            href={`/product/${product.slug}`}
             className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
           >
             <Image
