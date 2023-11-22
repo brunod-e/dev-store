b402cbd871af89aaba1bc974fc65ef372c9776b4
Bruno <brunodaniel.elias@gmail.com>
2023-11-21 21:43:26 -0300

    add: new command to cypress

--- cypress/e2e/search.cy.ts
+++ cypress/e2e/search.cy.ts
@@ -1,7 +1,6 @@
 describe("search products", () => {
   it("should be able to search for products", () => {
-    cy.visit("/")
-    cy.get("input[name='q']").type("Moletom").parent("form").submit()
+    cy.searchByQuery("Moletom")
 
     cy.location("pathname").should("include", "/search")
     cy.location("search").should("include", "q=Moletom")
